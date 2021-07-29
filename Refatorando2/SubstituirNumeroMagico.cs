36b01475a3a032eb8a431eb833d15c4a837475a7
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-28 21:41:29 -0300

    substituindo numero magico por constante

--- Refatorando2/SubstituirNumeroMagico.cs
+++ Refatorando2/SubstituirNumeroMagico.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Refatorando2
 {
-    class SubstituirNumeroMagico
+    partial class Program
     {
+        private const decimal ALIQUOTA_ICMS_PADRAO = 0.15m;
+        private const decimal ALIQUOTA_ICMS_SP = 0.18m;
+        private const string UF_SP = "SP";
+
+        public static decimal CalcularValor(decimal valorProdutos, string uf)
+        {
+            if (valorProdutos < 0)
+            {
+                throw new ArgumentOutOfRangeException("Valor não pode ser negativo ");
+            }
+            if (uf == UF_SP)
+            {
+                return valorProdutos * ALIQUOTA_ICMS_SP;
+            }
+            return valorProdutos * ALIQUOTA_ICMS_PADRAO;
+        }
     }
 }
