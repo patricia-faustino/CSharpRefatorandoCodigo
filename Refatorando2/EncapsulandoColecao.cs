9b9f36f893543d6603cb5060e262ba9967e266de
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-28 22:45:39 -0300

    Encapsulando colecoes

---
+++ Refatorando2/EncapsulandoColecao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refatorando2
+{
+
+    partial class Programa
+    {
+        void Testar()
+        {
+            Aluno aluno = new Aluno();
+            aluno.Adicionar(new Curso("JavaScript Básico"));
+            aluno.Adicionar(new Curso("C# Intermediário"));
+            aluno.Adicionar(new Curso("Java Avançado"));
+        }
+    }
+
+    class Aluno
+    {
+        private readonly List<Curso> cursos;
+        public IReadOnlyCollection<Curso> Cursos { get { return new ReadOnlyCollection<Curso>(cursos); } }
+
+        public Aluno()
+        {
+            cursos = new List<Curso>();
+        }
+
+        internal void Adicionar(Curso curso)
+        {
+            cursos.Add(curso);
+        }
+
+        internal void Remover(Curso curso)
+        {
+            cursos.Remove(curso);
+        }
+    }
+
+    class Curso
+    {
+        readonly string nome;
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public Curso(string nome)
+        {
+            this.nome = nome;
+        }
+    }
+}
