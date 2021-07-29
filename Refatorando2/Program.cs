e1f53aeae65230f7343b7ec1a1dec8bdfaf2011f
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-28 21:19:40 -0300

    Substituindo numneros magicos por constantes

---
+++ Refatorando2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refatorando2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valorImposto = ICMS.CalcularValor(1000, "SP");
+        }
+    }
+
+    class ICMS
+    {
+        private const decimal ALIQUOTA_ICMS_PADRAO = 0.15m;
+        private const decimal ALIQUOTA_ICMS_SP = 0.18m;
+        private const string UF_SP = "SP";
+
+        public static decimal CalcularValor(decimal valorProdutos, string uf)
+        {
+            if(valorProdutos < 0)
+            {
+                throw new ArgumentOutOfRangeException("Valor não pode ser");
+            }
+            if(uf == UF_SP)
+            {
+                return valorProdutos * ALIQUOTA_ICMS_SP;
+            }
+            return valorProdutos * ALIQUOTA_ICMS_PADRAO;
+        }
+    }
+}
