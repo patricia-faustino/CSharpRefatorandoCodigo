61f6d11155902649790282760757068fcfbebf98
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-28 21:41:43 -0300

    substindo array por objeto

--- Refatorando2/Program.cs
+++ Refatorando2/Program.cs
@@ -6,31 +6,12 @@ using System.Threading.Tasks;
 
 namespace Refatorando2
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            var valorImposto = ICMS.CalcularValor(1000, "SP");
+            TesteSubstituirArrayPorObjeto();
         }
     }
 
-    class ICMS
-    {
-        private const decimal ALIQUOTA_ICMS_PADRAO = 0.15m;
-        private const decimal ALIQUOTA_ICMS_SP = 0.18m;
-        private const string UF_SP = "SP";
-
-        public static decimal CalcularValor(decimal valorProdutos, string uf)
-        {
-            if(valorProdutos < 0)
-            {
-                throw new ArgumentOutOfRangeException("Valor não pode ser");
-            }
-            if(uf == UF_SP)
-            {
-                return valorProdutos * ALIQUOTA_ICMS_SP;
-            }
-            return valorProdutos * ALIQUOTA_ICMS_PADRAO;
-        }
-    }
 }
